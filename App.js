58a57655ce1d385bc048039abe8a56b4b42c6efa
sohil672007 <71943811+sohil672007@users.noreply.github.com>
2020-11-28 12:28:11 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Welcome from './screens/Welcome.js';
+
+export default class App extends React.Component{
+  render(){
+    return(
+      <Welcome/>    
+
+      
+    );
+  }
+}
